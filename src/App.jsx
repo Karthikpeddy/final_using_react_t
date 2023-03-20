09e472edaca681102c713fe15f41aaad8a015d53
Karthikpeddy <117649571+Karthikpeddy@users.noreply.github.com>
2023-03-20 17:52:25 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Spline from "@splinetool/react-spline";
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/ic9zzXov3Su4Ev1C/scene.splinecode" />
+  );
+}
